cc394fe2710a98b1eddf81cc68f412ce796ba792
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-09-01 23:54:08 +0800

    feat: Add popup on event markers

--- src/components/EventMarkers.js
+++ src/components/EventMarkers.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { CircleMarker } from "react-leaflet";
+import moment from 'moment';
+import { CircleMarker, Popup } from "react-leaflet";
 
 function toRadius(magnitude) {
   // 0th index is for mag<1, then mag=1+, and so on up to 33.32 for mag>8
@@ -14,7 +15,12 @@ const EventMarkers = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('http://localhost:5000/eventsList'); 
+      const result = await axios.get('http://192.168.1.12:5000/eventsList',
+        {params: {
+          startTime:moment('2021-01-01').format("YYYY-MM-DD HH:mm:ss"),
+          endTime:moment().format("YYYY-MM-DD HH:mm:ss"),
+        }}
+      ); 
       setEvents(result.data);
     };
     fetchData();
@@ -28,7 +34,19 @@ const EventMarkers = () => {
         key={event.publicID} 
         center={[event.latitude_value, event.longitude_value]}
         radius={toRadius(event.magnitude_value)}
-      />
+      >
+        <Popup>
+          <div>
+            <h2>Magnitude {+event.magnitude_value.toFixed(2)}</h2>
+            <p>{moment(event.OT).format(
+                            "YYYY-MM-DD hh:mm:ss A [(UTC]Z[)]")}</p>
+            <p>
+              {event.latitude_value.toFixed(3)}&#176;N&nbsp;
+              {event.longitude_value.toFixed(3)}&#176;E
+            </p>
+          </div>
+        </Popup>
+      </CircleMarker>
     )
   );
 }
