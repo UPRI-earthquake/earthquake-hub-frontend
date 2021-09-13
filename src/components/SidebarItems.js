f8b4b4d1d168b217dea18ae5f7a24fef8368bfb9
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-09-13 20:35:51 +0800

    style: Fix indentation

--- src/components/SidebarItems.js
+++ src/components/SidebarItems.js
@@ -1,15 +1,15 @@
 import SidebarItem from "./SidebarItem"
 
 function SidebarItems({initData}) {
-   return(initData.map(row => 
-     <SidebarItem 
-       key={row.publicID}
-       publicID={row.publicID}
-       title={+row.magnitude_value.toFixed(1)}
-       description={row.place}
-       subDescription={row.OT}
-     />
-   ))
+  return(initData.map(row => 
+    <SidebarItem 
+      key={row.publicID}
+      publicID={row.publicID}
+      title={+row.magnitude_value.toFixed(1)}
+      description={row.place}
+      subDescription={row.OT}
+    />
+  ))
 }
 
 export default SidebarItems
