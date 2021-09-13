2c23521f5913b2f3cf6b759f482c079941711ca8
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-09-13 20:31:58 +0800

    fix: Ensure initial data & connx availability before render

--- src/App.js
+++ src/App.js
@@ -12,61 +12,86 @@ import Header from "./components/Header";
 import SSEContext from "./SSEContext";
 
 const App = () => {
-  const [events, setEvents] = useState([]); // useState to persist data
+  // use loading screen (with min time) to wait for events and eventsSource
+  const [loading, setLoading] = useState(true) 
+
+  const eventsRef = useRef([]);  // initial eq-events data
+  const eventSourceRef = useRef(null) // SSE-emitter
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get('http://192.168.1.12:5000/eventsList',
-        {params: {
-          startTime:moment('2021-09-09 14:30:00.0').format("YYYY-MM-DD HH:mm:ss"),
-          endTime:moment().format("YYYY-MM-DD HH:mm:ss"),
-        }}
-      ); 
-      setEvents(result.data);
-    };
-    fetchData();
-  }, []);
+    // get initial eq-events from backend
+    const fetchPromise = axios.get('http://192.168.1.12:5000/eventsList',
+      {params: {
+        startTime:moment('2021-09-09 14:30:00.0').format("YYYY-MM-DD HH:mm:ss"),
+        endTime:moment().format("YYYY-MM-DD HH:mm:ss"),
+      }}
+    ); 
 
-  // subscribe to events channel
-  const eventSourceRef = useRef(null)
-  useEffect(() => {// assign ref to source, will persist across renders
-    eventSourceRef.current = new EventSource('http://192.168.1.12:5000/messaging')
-    const eventSource = eventSourceRef.current
+    // connect to an emitter for SSE
+    const eventSourcePromise = new Promise((resolve, reject) => {
+      const source = new EventSource('http://192.168.1.12:5000/messaging')
+      source ? resolve(source) : reject('EventSource connection error')
+    });
 
-    const onError = error => {
-      console.log(error)
-      eventSource.close()
-    }
+    // to wait at least timeoutPromise time before trigerring a render
+    const timeoutPromise = new Promise(resolve => {
+      setTimeout(resolve, 2000);
+    })
 
-    eventSource.addEventListener('error', onError);
+    Promise.all([fetchPromise, eventSourcePromise, timeoutPromise])
+      .then((values) => {
+        eventsRef.current = values[0].data//fetchResult.data
+
+        eventSourceRef.current = values[1]
+        eventSourceRef.current.addEventListener('error', (error) =>{
+          console.log(error)
+          eventSourceRef.current.close()
+        });
+
+        setLoading(false);
+
+      })
+      .catch(errorArray => console.log(errorArray))
 
     return () => {
       // clean up function 
-      eventSource.close()
+      if(eventSourceRef.current){
+        eventSourceRef.current.close()
+      }
       //eventSource.removeEventListener('error',onError)
+      //TODO: Remove all event listeners?
     }
   }, []);
 
   return (
-    <div className="App">
-      <Header />
-      <div className="App-body">
-        <SSEContext.Provider value={eventSourceRef.current}>
-          <Sidebar >
-            <SidebarInfo/>
-            <SidebarItems initData={events}/>
-          </Sidebar>
-          <MapContainer center={[12.2795, 122.049]} zoom={6}>
-            <TileLayer
-              url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            />
-            <EventMarkers events={events}/>
-            <StationMarkers />
-          </MapContainer>
-        </SSEContext.Provider>
+    <>
+    {loading ? (
+      <h2>
+        Loading...
+        {console.log('render loading screen')}
+      </h2>
+    ):(
+      <div className="App">
+        {console.log('render app screen')}
+        <Header />
+        <div className="App-body">
+          <SSEContext.Provider value={eventSourceRef.current}>
+            <Sidebar >
+              <SidebarInfo/>
+              <SidebarItems initData={eventsRef.current}/>
+            </Sidebar>
+            <MapContainer center={[12.2795, 122.049]} zoom={6}>
+              <TileLayer
+                url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+              />
+              <EventMarkers events={eventsRef.current}/>
+              <StationMarkers />
+            </MapContainer>
+          </SSEContext.Provider>
+        </div>
       </div>
-    </div>
-      
+    )};
+    </>
   );
 }
 
