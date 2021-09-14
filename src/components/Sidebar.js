59497d65d3c958f0a85ee3ddd6c3a795e2810bc4
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-09-14 23:08:17 +0800

    feat: Receive and act on server-sent events

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,3 @@
-import React from "react"
 import styles from "./Sidebar.module.css"
 
 function Sidebar(props) {
--- src/components/SidebarItems.js
+++ src/components/SidebarItems.js
@@ -1,13 +1,61 @@
+import React, {useState, useContext, useEffect} from "react"
 import SidebarItem from "./SidebarItem"
+import SSEContext from "../SSEContext";
 
 function SidebarItems({initData}) {
-  return(initData.map(row => 
+  const [items, setItems] = useState(initData.sort((a, b)=>{
+    return new Date(b.OT) - new Date(a.OT);
+  }))
+
+  const eventSource = useContext(SSEContext); 
+  useEffect(() => {
+    const handleEQEvent = (event) => {
+      console.log(`SIDEBAR: Received EQ Event`)
+      const data = JSON.parse(event.data)// to parse to get valid json-obj
+      console.log(items)
+
+      switch (data.eventType){
+        case 'NEW':
+          setItems([{
+            publicID: data.publicID,
+            magnitude_value: data.magnitude_value,
+            place: data.place,
+            OT: data.OT,
+            text: data.text,
+          }, ...items])
+          break;
+        case 'UPDATE':
+          setItems(items.map(item=>{
+            if (item.publicID === data.publicID){
+              return {
+                publicID: data.publicID,
+                magnitude_value: data.magnitude_value,
+                place: data.place,
+                OT: data.OT,
+                text: data.text,
+              }
+            }else{ return item }
+          }));
+          break;
+        default:
+          ;
+      }
+    }
+    eventSource.addEventListener('SC_EVENT', handleEQEvent);
+
+    return () => {
+      eventSource.removeEventListener('SC_EVENT', handleEQEvent);
+    };
+  }, [eventSource, items]);
+
+  return(items.map(item => 
     <SidebarItem 
-      key={row.publicID}
-      publicID={row.publicID}
-      title={+row.magnitude_value.toFixed(1)}
-      description={row.place}
-      subDescription={row.OT}
+      key={item.publicID}
+      publicID={item.publicID}
+      title={+item.magnitude_value.toFixed(1)}
+      description={item.place !== 'Nominatim unavailable' 
+                    ? item.place : item.text}
+      subDescription={item.OT}
     />
   ))
 }
