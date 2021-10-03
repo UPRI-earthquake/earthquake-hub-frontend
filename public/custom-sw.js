46389f93c6ae8266b659e85507434ef9e0634a3a
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2021-10-03 23:04:51 +0800

    feat: Add notif-badge and notif-image

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -4,6 +4,8 @@ self.addEventListener('push', event => {
   console.log('New notification', data)
   const options = {
     body: data.body,
+    badge: './badge-92x92.png' ,
+    icon: './android-chrome-192x192.png',
   }
   event.waitUntil(
     self.registration.showNotification(data.title, options)
