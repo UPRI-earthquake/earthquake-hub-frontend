25740f0831800da9a601dd1325f9dcd0e5731c27
Jeff Sanchez (sysop@hp) <sanchez.christopherjeff@gmail.com>
2022-01-28 06:39:18 +0800

    refactor: Change number of days

--- src/components/SidebarInfo.js
+++ src/components/SidebarInfo.js
@@ -4,7 +4,7 @@ import styles from "./SidebarInfo.module.css"
 function SidebarInfo() {
   return (
     <div className={styles.sidebarInfo}>
-      <h3>Latest Earthquakes, Past 3 Days</h3>
+      <h3>Latest Earthquakes, Past 30 Days</h3>
     </div>
   )
 }
