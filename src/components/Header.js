74a302f4ed0943f22ceacd02e432d159b48df3c6
chrscbs <johnchristiancabang@gmail.com>
2023-05-18 14:00:44 +0800

    feat: add transitions on successful login/signup, add burger menu icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,36 @@ import styles from "./Header.module.css";
 import {ReactComponent as Logo} from './upri-logo.svg';
 import Button from "./Button";
 import { SignInForm, SignUpForm } from "./Form";
+import { Dashboard } from "./Dashboard";
+import { ReactComponent as BurgerMenu } from './burger-menu-white.svg';
 
 function Header() {
   const [showSignInForm, setShowSignInForm] = useState(false);
   const [showSignUpForm, setShowSignUpForm] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showDashboard, setShowDashboard] = useState(false);
+  const [isDashboardOpen, setIsDashboardOpen] = useState(false);
 
   const handleSignInClick = () => setShowSignInForm(true);
   const handleSignInClose = () => setShowSignInForm(false);
   const handleSignUpClick = () => setShowSignUpForm(true);
   const handleSignUpClose = () => setShowSignUpForm(false);
+
+  const handleSignInSuccess = () => {
+    setShowSignInForm(false);
+    setShowDashboard(true);
+    setIsLoggedIn(true); // User is now logged in
+  }
+  const handleSignUpSuccess = () => {
+    setShowSignUpForm(false);
+    setShowDashboard(true);
+    setIsLoggedIn(true); // User is now logged in
+  }
+
+  const handleDashboardToggle = () => {
+    setIsDashboardOpen(!isDashboardOpen);
+    setShowDashboard(!isDashboardOpen)
+  };
 
   return(
     <div className={styles.header}>
@@ -21,16 +42,23 @@ function Header() {
         <h1>CS•UPRI</h1>
       </div>
       <div className={styles.headerRight}>
-        <Button hasOutline={false} onClick={handleSignInClick}>
-          Sign in
-        </Button>
-        <Button hasOutline={true} onClick={handleSignUpClick}>
-          Sign up
-        </Button>
+          {isLoggedIn ? (
+            <BurgerMenu className={styles.burgerMenu} onClick={handleDashboardToggle} />
+          ) : (
+            <>
+              <Button hasOutline={false} onClick={handleSignInClick}>
+                Sign in
+              </Button>
+              <Button hasOutline={true} onClick={handleSignUpClick}>
+                Sign up
+              </Button>
+            </>
+          )}
       </div>
     </div>
-      {showSignInForm && <SignInForm onClick={handleSignInClose} />}
-      {showSignUpForm && <SignUpForm onClick={handleSignUpClose} />}
+      {showSignInForm && <SignInForm onClick={handleSignInClose} onSuccess={handleSignInSuccess} />}
+      {showSignUpForm && <SignUpForm onClick={handleSignUpClose} onSuccess={handleSignUpSuccess} />}
+      {showDashboard && <Dashboard onClick={handleDashboardToggle} onEscapeClick={handleDashboardToggle} />}
     </div>
   )
 }
