12cb94b2c9f15f00b78eb91680eefb160e26a9f2
chrscbs <johnchristiancabang@gmail.com>
2023-07-12 01:25:00 +0800

    feat: add function that traces the data coming from a remote ringserver

--- src/components/StationMarker.js
+++ src/components/StationMarker.js
@@ -7,8 +7,129 @@ import styles from './StationMarker.module.css'
 import SSEContext from "../SSEContext";
 import moment from 'moment';
 import axios from 'axios';
+import * as sp from 'seisplotjs';
 
 const StationMarker = ({code, latLng, description}) => {
+    
+  const realtimeDivRef = useRef(null);
+  const graphListRef = useRef(new Map());
+  const numPacketsRef = useRef(0);
+  const redrawInProgressRef = useRef(false);
+  const datalinkRef = useRef(null);
+
+  function getRealTimeTrace(){
+    const matchPattern = 'CO_BIRD_00_HHZ/MSEED';
+    const duration = sp.luxon.Duration.fromISO('PT3M');
+    const timeWindow = new sp.util.durationEnd(duration, sp.luxon.DateTime.utc());
+    const seisPlotConfig = new sp.seismographconfig.SeismographConfig();
+    seisPlotConfig.wheelZoom = false;
+    seisPlotConfig.isYAxisNice = true;
+    seisPlotConfig._yLabel = 'Amplitude';
+    seisPlotConfig.linkedTimeScale.offset = sp.luxon.Duration.fromMillis(-1.2 * duration.toMillis()); // BINAGO KO TO FROM -1 to -1.2
+    seisPlotConfig.linkedTimeScale.duration = duration;
+    seisPlotConfig.linkedAmplitudeScale = new sp.scale.IndividualAmplitudeScale();
+    seisPlotConfig.doGain = true;
+
+    const packetHandler = function (packet) {
+      if (packet.isMiniseed()) {
+        numPacketsRef.current++;
+        let seisSegment = sp.miniseed.createSeismogramSegment(packet.asMiniseed());
+        const codes = seisSegment.codes();
+        let seisPlot = graphListRef.current.get(codes);
+        if (!seisPlot) {
+          let seismogram = new sp.seismogram.Seismogram([seisSegment]);
+          let seisData = sp.seismogram.SeismogramDisplayData.fromSeismogram(seismogram);
+          seisData.alignmentTime = sp.luxon.DateTime.utc();
+          seisPlot = new sp.seismograph.Seismograph([seisData], seisPlotConfig);
+          realtimeDivRef.current.appendChild(seisPlot);
+          graphListRef.current.set(codes, seisPlot);
+          console.log(`new plot: ${codes}`)
+        } else {
+          seisPlot.seisData[0].append(seisSegment);
+          seisPlot.recheckAmpScaleDomain();
+        }
+        seisPlot.draw();
+      } else {
+        console.log(`not a mseed packet: ${packet.streamId}`)
+      }
+    };
+
+    const errorFn = function (error) {
+      console.assert(false, error);
+      if (datalinkRef.current) { datalinkRef.current.close(); }
+    };
+
+    datalinkRef.current = new sp.datalink.DataLinkConnection(
+      "wss://rtserve.iris.washington.edu/datalink",
+      packetHandler,
+      errorFn
+    );
+    console.log(datalinkRef.current)
+
+    let timer;
+    const timerInterval = duration.toMillis() /
+      (realtimeDivRef.current.offsetWidth - seisPlotConfig.margin.left - seisPlotConfig.margin.right);
+    const drawGraph = function (timestamp) {
+      if (redrawInProgressRef.current) return;
+      redrawInProgressRef.current = true;
+      window.requestAnimationFrame(() => {
+        const now = sp.luxon.DateTime.utc();
+        graphListRef.current.forEach(function (graph) {
+          graph.seisData.forEach(sdd => {
+            sdd.alignmentTime = now;
+          });
+          graph.calcTimeScaleDomain();
+          graph.calcAmpScaleDomain();
+          graph.draw();
+        });
+        redrawInProgressRef.current = false;
+      });
+    };
+
+    const toggleConnect = function () {
+      if (datalinkRef.current) {
+        datalinkRef.current.connect()
+          .then(serverId => {
+            return datalinkRef.current.match(matchPattern);
+          }).then(response => {
+            if (response.isError()) {
+              console.log(`response is not OK, ignore... ${response}`);
+            }
+            return datalinkRef.current.infoStatus();
+          }).then(response => {
+            return datalinkRef.current.infoStreams();
+          }).then(response => {
+            return datalinkRef.current.positionAfter(timeWindow.start);
+          }).then(response => {
+            if (response.isError()) {
+              console.log(`Oops, positionAfter response is not OK, ignore... ${response}`);
+            }
+            return datalinkRef.current.stream();
+          }).catch(function (error) {
+            console.log("Error: " + error);
+            console.assert(false, error);
+          });
+      }
+    };
+
+    const startGraph = function () {
+      timer = window.setInterval(drawGraph, timerInterval);
+      toggleConnect();
+    };
+
+    // const stopGraph = function () {
+    //   if (timer) {
+    //     clearInterval(timer);
+    //     timer = null;
+    //   }
+    //   if (datalinkRef.current) {
+    //     datalinkRef.current.endStream();
+    //     datalinkRef.current.close();
+    //   }
+    // };
+
+    startGraph();
+  }
 
   const [pick, setPick] = useState(false);
   const timerId = useRef(null); // hold running timeout-id across renders
@@ -54,6 +175,8 @@ const StationMarker = ({code, latLng, description}) => {
         status: payload.status,
         statusSince: payload.statusSince
       })
+      getRealTimeTrace(); // call the function that traces the realtime data received by the ringserver 
+
     } catch (error) {
       console.error('Error occurred while fetching device status:', error);
       setStatusState({ status: null, statusSince: null });
@@ -84,9 +207,10 @@ const StationMarker = ({code, latLng, description}) => {
           <h3>Station {code}</h3>
           <p>{description}</p>
           <p>
-          {statusState.status && statusState.status} 
-          {statusState.statusSince && ` since ${moment(statusState.statusSince).fromNow()}`}
+            {statusState.status && statusState.status} 
+            {statusState.statusSince && ` since ${moment(statusState.statusSince).fromNow()}`}
           </p>
+          <div ref={realtimeDivRef}></div>
           <a href={data_download_URL} target="_blank" rel="noreferrer">Get past 24hrs data</a><br/>
           <a href={metadata_download_URL} target="_blank" rel="noreferrer">Get station metadata</a><br/>
         </div>
