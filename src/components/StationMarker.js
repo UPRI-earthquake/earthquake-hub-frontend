113cdc822dcfe9de2b1c5eb21936aa3726667d4c
prokorpio@adride <sanchez.christopherjeff@gmail.com>
2023-07-13 22:55:22 +0800

    refactor: take out toggleConnect function

--- src/components/StationMarker.js
+++ src/components/StationMarker.js
@@ -15,119 +15,121 @@ const StationMarker = ({network, code, latLng, description}) => {
   const graphListRef = useRef(new Map());
   const redrawInProgressRef = useRef(false);
   const datalinkRef = useRef(null);
+  const ringserver_ws = process.env.NODE_ENV === 'production'
+                       ? process.env.REACT_APP_RINGSERVER_WS
+                       : process.env.REACT_APP_RINGSERVER_WS_DEV
 
-  function getRealTimeTrace(net, sta){
-    const matchPattern = `${net}_${sta}_([0-9]{2})?_.HZ/MSEED`; 
-    const duration = sp.luxon.Duration.fromObject({ minutes: 2, seconds: 45 });
-    const graphDuration = sp.luxon.Duration.fromObject({ minutes: 2, seconds: 30 }); // Set the graphDuration 30 seconds shorter than the duration to make the trace look more realtime
-    const timeWindow = new sp.util.durationEnd(duration, sp.luxon.DateTime.utc());
-    const seisPlotConfig = new sp.seismographconfig.SeismographConfig();
-    seisPlotConfig.wheelZoom = false;
-    seisPlotConfig.linkedTimeScale.offset = sp.luxon.Duration.fromMillis(-1 * duration.toMillis());
-    seisPlotConfig.linkedTimeScale.duration = graphDuration;
-    seisPlotConfig.linkedAmplitudeScale = new sp.scale.IndividualAmplitudeScale();  
-    seisPlotConfig.doGain = true;
-    seisPlotConfig.isRelativeTime = true; // Display the time to be relative from the current time (in millis)
-    seisPlotConfig.xLabel = 'Time (seconds)';
+  /* Graph data from DataLink WebSocket */
+  const duration = sp.luxon.Duration.fromObject({ minutes: 2, seconds: 45 });
+  const graphDuration = sp.luxon.Duration.fromObject({ minutes: 2, seconds: 30 }); // Set the graphDuration 30 seconds shorter than the duration to make the trace look more realtime
+  const timeWindow = new sp.util.durationEnd(duration, sp.luxon.DateTime.utc());
+  const seisPlotConfig = new sp.seismographconfig.SeismographConfig();
+  seisPlotConfig.wheelZoom = false;
+  seisPlotConfig.linkedTimeScale.offset = sp.luxon.Duration.fromMillis(-1 * duration.toMillis());
+  seisPlotConfig.linkedTimeScale.duration = graphDuration;
+  seisPlotConfig.linkedAmplitudeScale = new sp.scale.IndividualAmplitudeScale();  
+  seisPlotConfig.doGain = true;
+  seisPlotConfig.isRelativeTime = true; // Display the time to be relative from the current time (in millis)
+  seisPlotConfig.xLabel = 'Time (seconds)';
 
-    const packetHandler = function (packet) {
-      if (packet.isMiniseed()) {
-        let seisSegment = sp.miniseed.createSeismogramSegment(packet.asMiniseed()); // Create a SeismogramSegment from the packet
-        let codes = seisSegment.codes(); // Get the codes (stream ID) of the SeismogramSegment
-        let seisPlot = graphListRef.current.get(codes); // Retrieve the corresponding graph for the codes from the graphListRef
+  const packetHandler = function (packet) {
+    if (packet.isMiniseed()) {
+      let seisSegment = sp.miniseed.createSeismogramSegment(packet.asMiniseed()); // Create a SeismogramSegment from the packet
+      let codes = seisSegment.codes(); // Get the codes (stream ID) of the SeismogramSegment
+      let seisPlot = graphListRef.current.get(codes); // Retrieve the corresponding graph for the codes from the graphListRef
 
-        if (!seisPlot) { // If the graph doesn't exist
-          // seisPlotConfig.title = codes; // Set the title of the graph based on the stream_id
+      if (!seisPlot) { // If the graph doesn't exist
+        // seisPlotConfig.title = codes; // Set the title of the graph based on the stream_id
 
-          let seismogram = new sp.seismogram.Seismogram([seisSegment]); // Create a Seismogram with the SeismogramSegment
-          let seisData = sp.seismogram.SeismogramDisplayData.fromSeismogram(seismogram); // Create SeismogramDisplayData from the Seismogram
-          seisData.alignmentTime = sp.luxon.DateTime.utc(); // Set the alignment time to current UTC time
+        let seismogram = new sp.seismogram.Seismogram([seisSegment]); // Create a Seismogram with the SeismogramSegment
+        let seisData = sp.seismogram.SeismogramDisplayData.fromSeismogram(seismogram); // Create SeismogramDisplayData from the Seismogram
+        seisData.alignmentTime = sp.luxon.DateTime.utc(); // Set the alignment time to current UTC time
 
-          seisPlot = new sp.seismograph.Seismograph([seisData], seisPlotConfig); // Create a new Seismograph with the SeismogramDisplayData and SeismographConfig
-          realtimeDivRef.current.appendChild(seisPlot); // Append the Seismograph to the realtimeDiv
-          graphListRef.current.set(codes, seisPlot); // Store the Seismograph in the graphListRef for future reference
+        seisPlot = new sp.seismograph.Seismograph([seisData], seisPlotConfig); // Create a new Seismograph with the SeismogramDisplayData and SeismographConfig
+        realtimeDivRef.current.appendChild(seisPlot); // Append the Seismograph to the realtimeDiv
+        graphListRef.current.set(codes, seisPlot); // Store the Seismograph in the graphListRef for future reference
 
-          console.log(`new plot: ${codes}`)
-        } else {
-          seisPlot.seisData[0].append(seisSegment);
-          seisPlot.recheckAmpScaleDomain();
-        }
-        seisPlot.draw();
+        console.log(`new plot: ${codes}`)
       } else {
-        console.log(`not a mseed packet: ${packet.streamId}`)
+        seisPlot.seisData[0].append(seisSegment);
+        seisPlot.recheckAmpScaleDomain();
       }
-    };
+      seisPlot.draw();
+    } else {
+      console.log(`not a mseed packet: ${packet.streamId}`)
+    }
+  };
 
-    const errorFn = function (error) {
-      console.assert(false, error);
-      if (datalinkRef.current) { datalinkRef.current.close(); } // Close the websocket connection
-    };
+  const errorFn = function (error) {
+    console.assert(false, error);
+    if (datalinkRef.current) { datalinkRef.current.close(); } // Close the websocket connection
+  };
 
-    /***************************************************************************
-     * new DataLinkConnection:
-     *     A websocket based Datalink connection
-     * Parameters:  
-     *     url            (string)                                    websocket url to the ringserver
-     *     packetHandler  (function (packet: DataLinkPacket): void)   callback for packets as they arrive
-     *     errorHandler   (function (error: Error): void)             callback for errors
-     *     
-     ***************************************************************************/
-    datalinkRef.current = new sp.datalink.DataLinkConnection(
-      ringserver_ws,
-      packetHandler,
-      errorFn
-    );
+  /***************************************************************************
+   * new DataLinkConnection:
+   *     A websocket based Datalink connection
+   * Parameters:  
+   *     url            (string)                                    websocket url to the ringserver
+   *     packetHandler  (function (packet: DataLinkPacket): void)   callback for packets as they arrive
+   *     errorHandler   (function (error: Error): void)             callback for errors
+   *     
+   ***************************************************************************/
+  datalinkRef.current = new sp.datalink.DataLinkConnection(
+    ringserver_ws,
+    packetHandler,
+    errorFn
+  );
 
+  const drawGraph = function () {
+    if (redrawInProgressRef.current) return; // Skip if redraw is already in progress
+    redrawInProgressRef.current = true; // Mark redraw as in progress
+    window.requestAnimationFrame(() => {
+      const now = sp.luxon.DateTime.utc();
+      graphListRef.current.forEach(function (graph) {
+        graph.seisData.forEach(sdd => {
+          sdd.alignmentTime = now;
+        });
+        graph.calcTimeScaleDomain(); // Recalculate time scale domain
+        graph.calcAmpScaleDomain(); // Recalculate amplitude scale domain
+        graph.draw();
+      });
+      redrawInProgressRef.current = false; // Mark redraw as complete
+    });
+  };
+
+  const toggleConnect = async function (network, station) {
+    if (datalinkRef.current) {
+      try {
+        const matchPattern = `${network}_${station}_([0-9]{2})?_.HZ/MSEED`; 
+
+        await datalinkRef.current.connect(); // Create websocket connection and send the client ID
+        const matchResponse = await datalinkRef.current.match(matchPattern); // Send match command
+        if (matchResponse.isError()) {
+          console.log(`response is not OK, ignore... ${matchResponse}`);
+        }
+
+        const positionResponse = await datalinkRef.current.positionAfter(timeWindow.start); // Send position after match command
+        if (positionResponse.isError()) {
+          console.log(`Oops, positionAfter response is not OK, ignore... ${positionResponse}`);
+        }
+  
+        await datalinkRef.current.stream(); // Switch to streaming mode to receive data packets from the ringserver
+      } catch (error) {
+        console.log("Error: " + error);
+        console.assert(false, error);
+      }
+    }
+  };
+
+  const startGraph = function (network, station) {
     const timerInterval = duration.toMillis() /
       (realtimeDivRef.current.offsetWidth - seisPlotConfig.margin.left - seisPlotConfig.margin.right);
 
-    const drawGraph = function () {
-      if (redrawInProgressRef.current) return; // Skip if redraw is already in progress
-      redrawInProgressRef.current = true; // Mark redraw as in progress
-      window.requestAnimationFrame(() => {
-        const now = sp.luxon.DateTime.utc();
-        graphListRef.current.forEach(function (graph) {
-          graph.seisData.forEach(sdd => {
-            sdd.alignmentTime = now;
-          });
-          graph.calcTimeScaleDomain(); // Recalculate time scale domain
-          graph.calcAmpScaleDomain(); // Recalculate amplitude scale domain
-          graph.draw();
-        });
-        redrawInProgressRef.current = false; // Mark redraw as complete
-      });
-    };
+    window.setInterval(drawGraph, timerInterval);
+    toggleConnect(network, station);
+  };
+  /* End of graph data from DataLink WebSocket */
 
-    const toggleConnect = async function () {
-      if (datalinkRef.current) {
-        try {
-          await datalinkRef.current.connect(); // Create websocket connection and send the client ID
-
-          const matchResponse = await datalinkRef.current.match(matchPattern); // Send match command
-          if (matchResponse.isError()) {
-            console.log(`response is not OK, ignore... ${matchResponse}`);
-          }
-
-          const positionResponse = await datalinkRef.current.positionAfter(timeWindow.start); // Send position after match command
-          if (positionResponse.isError()) {
-            console.log(`Oops, positionAfter response is not OK, ignore... ${positionResponse}`);
-          }
-    
-          await datalinkRef.current.stream(); // Switch to streaming mode to receive data packets from the ringserver
-        } catch (error) {
-          console.log("Error: " + error);
-          console.assert(false, error);
-        }
-      }
-    };
-
-    const startGraph = function () {
-      window.setInterval(drawGraph, timerInterval);
-      toggleConnect();
-    };
-
-    startGraph();
-  }
 
   const [pick, setPick] = useState(false);
   const timerId = useRef(null); // hold running timeout-id across renders
@@ -136,9 +138,6 @@ const StationMarker = ({network, code, latLng, description}) => {
   const backend_host = process.env.NODE_ENV === 'production'
                        ? process.env.REACT_APP_BACKEND
                        : process.env.REACT_APP_BACKEND_DEV
-  const ringserver_ws = process.env.NODE_ENV === 'production'
-                       ? process.env.REACT_APP_RINGSERVER_WS
-                       : process.env.REACT_APP_RINGSERVER_WS_DEV
 
   useEffect(() => {
     const handlePickEvent = (event) => {
@@ -176,7 +175,7 @@ const StationMarker = ({network, code, latLng, description}) => {
         status: payload.status,
         statusSince: payload.statusSince
       })
-      getRealTimeTrace(network, code); // Call the function that traces the realtime data received by the ringserver
+      startGraph(network, code);
     } catch (error) {
       console.error('Error occurred while fetching device status:', error);
       setStatusState({ status: null, statusSince: null });
@@ -199,7 +198,8 @@ const StationMarker = ({network, code, latLng, description}) => {
       position={latLng}
       icon={divTriangle}
       eventHandlers={{
-        click: handleStationClick
+        click: handleStationClick,
+        popupclose: (e) => {console.table(e)}
       }}
     >
       <Popup className={styles.popUp}>
