624ac4ab17a4c50520a5e4f8032e62ca3ec51050
chrscbs <johnchristiancabang@gmail.com>
2023-08-30 17:25:58 +0800

    hotfix: add ringserverPort input, correction

--- src/components/Form.js
+++ src/components/Form.js
@@ -152,7 +152,7 @@ function SignUpForm( {onClick, onSuccess} ) {
       if (role === 'brgy') {
         // ringserverUrl format = url:port
         const ringserverUrl = event.target.elements.ringserverUrl.value;
-        const ringserverPort = events.target.elements.ringserverPort.value;
+        const ringserverPort = event.target.elements.ringserverPort.value;
         requestPayload.ringserverUrl = ringserverUrl;
         requestPayload.ringserverPort = ringserverPort;
       }
