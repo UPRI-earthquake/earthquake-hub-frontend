d7aa1f4d1377e4778383b021d832f23f573e5f97
chrscbs <johnchristiancabang@gmail.com>
2024-07-31 14:21:51 +0800

    feat: add new endpoints in frontend; this pages will be utilized by the interns

---
+++ src/pages/EQInfoPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EQInfoPage() {
+    return (
+        <div>
+            <h1>This is the EQInfo Page --for Kim</h1>
+        </div>
+    );
+}
+
+export default EQInfoPage;
---
+++ src/pages/SignificantEQsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignificantEQsPage() {
+    return (
+        <div>
+            <h1>This is the Significant Earthquake Page --for Lyward</h1>
+        </div>
+    );
+}
+
+export default SignificantEQsPage;
