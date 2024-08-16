b0645dcfc6ef77edf911a23c5dfcb99bb1ee03aa
Lyward Manuel S. Tongzon <lstongzon@up.edu.ph>
2024-08-16 20:15:30 +0800

    Added frontend of significant-eq-page

--- src/pages/SignificantEQsPage.js
+++ src/pages/SignificantEQsPage.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import Header from "./components/Header";
+import Card from "./components/Card";
+import './SignificantEQsPage.css';
 
-function SignificantEQsPage() {
-    return (
-        <div>
-            <h1>This is the Significant Earthquake Page --for Lyward</h1>
+function App() {
+  const cardsData = [
+    { title: 'Magnitude 7.1 Earthquake in Sultan Kudarat', magnitude: '7.1', location: '99 km S 72° W of Palimbang (Sultan Kudarat)', date: '11 July 2024', time: '10:13 AM', 
+      description: 'Caused extensive structural damage to buildings, leading to collapsed walls, cracked foundations, and compromised safety. Infrastructure such as roads and bridges suffered significant disruption, impeding emergency response and daily commutes.' },
+    { title: 'Magnitude 7.1 Earthquake in Sultan Kudarat', magnitude: '7.1', location: '99 km S 72° W of Palimbang (Sultan Kudarat)', date: '11 July 2024', time: '10:13 AM', 
+      description: 'Caused extensive structural damage to buildings, leading to collapsed walls, cracked foundations, and compromised safety. Infrastructure such as roads and bridges suffered significant disruption, impeding emergency response and daily commutes.' },
+    { title: 'Magnitude 7.1 Earthquake in Sultan Kudarat', magnitude: '7.1', location: '99 km S 72° W of Palimbang (Sultan Kudarat)', date: '11 July 2024', time: '10:13 AM', 
+      description: 'Caused extensive structural damage to buildings, leading to collapsed walls, cracked foundations, and compromised safety. Infrastructure such as roads and bridges suffered significant disruption, impeding emergency response and daily commutes.' },
+    { title: 'Magnitude 7.1 Earthquake in Sultan Kudarat', magnitude: '7.1', location: '99 km S 72° W of Palimbang (Sultan Kudarat)', date: '11 July 2024', time: '10:13 AM', 
+      description: 'Caused extensive structural damage to buildings, leading to collapsed walls, cracked foundations, and compromised safety. Infrastructure such as roads and bridges suffered significant disruption, impeding emergency response and daily commutes.' },
+    { title: 'Magnitude 7.1 Earthquake in Sultan Kudarat', magnitude: '7.1', location: '99 km S 72° W of Palimbang (Sultan Kudarat)', date: '11 July 2024', time: '10:13 AM', 
+      description: 'Caused extensive structural damage to buildings, leading to collapsed walls, cracked foundations, and compromised safety. Infrastructure such as roads and bridges suffered significant disruption, impeding emergency response and daily commutes.' },
+    { title: 'Magnitude 7.1 Earthquake in Sultan Kudarat', magnitude: '7.1', location: '99 km S 72° W of Palimbang (Sultan Kudarat)', date: '11 July 2024', time: '10:13 AM', 
+      description: 'Caused extensive structural damage to buildings, leading to collapsed walls, cracked foundations, and compromised safety. Infrastructure such as roads and bridges suffered significant disruption, impeding emergency response and daily commutes.' },
+    { title: 'Magnitude 7.1 Earthquake in Sultan Kudarat', magnitude: '7.1', location: '99 km S 72° W of Palimbang (Sultan Kudarat)', date: '11 July 2024', time: '10:13 AM', 
+      description: 'Caused extensive structural damage to buildings, leading to collapsed walls, cracked foundations, and compromised safety. Infrastructure such as roads and bridges suffered significant disruption, impeding emergency response and daily commutes.' },
+  ];
+
+  return (
+    <div className="App">
+      {console.log('render app screen')}
+        <Header />
+        <div className="App-body">
+          <div className="content-container">
+            <h1>Significant Earthquakes</h1>
+            <div className="cards-container">
+              {cardsData.map((card, index) => (
+                <Card key={index} title={card.title} magnitude={card.magnitude} location={card.location} date={card.date} time={card.time} description={card.description} />
+              ))}
+          </div>
+          </div>
         </div>
-    );
+    </div>
+  );
 }
 
-export default SignificantEQsPage;
+export default App;
