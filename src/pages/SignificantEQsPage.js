7f1e6f2908da27920d5d9a5a89f03210b6dddc77
chrscbs <johnchristiancabang@gmail.com>
2024-09-16 13:20:44 +0800

    chore: rename css file

--- src/pages/SignificantEQsPage.js
+++ src/pages/SignificantEQsPage.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Card from "../components/Card";
-import './significantEQsPage.css';
+import './SignificantEQsPage.css';
 
 function App() {
   const cardsData = [
