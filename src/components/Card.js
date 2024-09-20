5872044fe5b2541c64cdfc4b5be836f245f2666c
chrscbs <johnchristiancabang@gmail.com>
2024-09-20 09:47:42 +0800

    chore: make card title smaller

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ title, magnitude, location, date, time, description, onClick }) 
   return (
     <div className="card" onClick={onClick}>
       <div className="card-content">
-        <h3>{title}</h3>
+        <h4>{title}</h4>
         <div className="hover-details">
           <h4>Magnitude {magnitude}</h4>
           <h5>{location}</h5>
